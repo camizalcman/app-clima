decaa075a90ccb805f190ecad2381a76d3d2c788
CamilaZalcman <camizalcman@gmail.com>
2025-10-15 12:40:17 -0300

    array ropa

--- src/components/Vestimenta.vue
+++ src/components/Vestimenta.vue
@@ -7,12 +7,27 @@ const props = defineProps({
   clima:Object
 })
 
-
+const ropaFrio = [
+  { prenda: "Bufanda", img: "https://example.com/bufanda.png" },
+  { prenda: "Guantes", img: "https://example.com/guantes.png" },
+  { prenda: "Gorro", img: "https://example.com/gorro.png" },
+  { prenda: "Suéter", img: "https://example.com/sueter.png" },
+  { prenda: "Remera manga larga", img: "https://example.com/remera_manga_larga.png" },
+  { prenda: "Campera", img: "https://example.com/campera.png" },
+  { prenda: "Pantalones térmicos", img: "https://example.com/pantalones_termicos.png" }
+];
 </script>
 
 <template>
     <div v-if="clima">
-
+        <h2>Ropa recomendada para clima frío:</h2>
+        <div class="ropa-frio">
+            <div v-for="(item, index) in ropaFrio" :key="index" class="item">
+                <img :src="item.img" :alt="item.prenda" width="100" height="100"/>
+                <p>{{ item.prenda }}</p>
+            </div>
+        </div>
+      
   </div>
 </template>
 
